49ea81101ab52e82ed616a54dd992c8f7f899a61
jeanpix244 <42078602+jeanpix244@users.noreply.github.com>
2025-09-23 16:22:03 -0700

    Update index.js
    change color back to green (because blue colored links did not pass chromatic congruence test with red background )

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -7,7 +7,7 @@ import Link from 'next/link';
 
 export default function Home({ allPostsData }) {
   return (
-    <div style={{ backgroundColor: 'red', minHeight: '100vh', padding: '1rem' }}>
+    <div style={{ backgroundColor: 'green', minHeight: '100vh', padding: '1rem' }}>
       <Layout home>
         <Head>
           <title>{siteTitle}</title>
